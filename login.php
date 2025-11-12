e8728354c67c8b15a625ad77c4f7427dde687034
Akash Suttakoti <asuttakoti@gmail.com>
2025-11-12 13:18:45 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,34 @@
-<?
-session_start()
+
+
+<?php
+session_start();
+require("dbcon.php");
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM users WHERE username='$username' and password='$password'";
+    $res = mysqli_query($con, $sql);
+    if (mysqli_num_rows($res) > 0) {
+
+        while ($row = mysqli_fetch_assoc($res)) {
+            $_SESSION["username"] = $row['username'];
+            $_SESSION['role'] = $row['role'];
+
+            if ($row['role'] == 'admin') {
+                header('Location:adminhome.php');
+                exit;
+            } elseif ($row['role'] == 'user') {
+                header('Location:userhome.php');
+                exit;
+            }
+
+        }
+    } else {
+        echo "invaled username or password...:(";
+    }
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,7 +71,7 @@ session_start()
         <h2>Sign-in Page</h2>
       </div>
       <div class="card-body">
-        <form action="code.php" method="post">
+        <form method="post">
           <div class="mb-3">
             <label for="username" class="form-label">Username:</label>
             <input type="text" id="username" class="form-control" required name="username">
