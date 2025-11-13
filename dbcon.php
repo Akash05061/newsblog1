334eb89bda6a77c2beafda0c2ed101f1c32e1d32
Akash Suttakoti <asuttakoti@gmail.com>
2025-11-13 11:29:56 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,8 +1,8 @@
 <?php
 // Hardcode the NEW, REAL credentials
-$rds_host = "your-NEW-db.xxxxxx.rds.amazonaws.com";
-$rds_user = "your-new-admin-user";
-$rds_pass = "your-new-password";
+$rds_host = "database-1.cb80q24w8pym.us-east-1.rds.amazonaws.com";
+$rds_user = "root";
+$rds_pass = "aaaaaaaa";
 $rds_db   = "blog";
 
 $con = mysqli_connect($rds_host, $rds_user, $rds_pass, $rds_db);
